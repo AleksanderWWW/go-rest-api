d61d637b71c1f98df93f15a6fcd97088a506a238
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-01-30 12:15:39 +0100

    add logic to switcxh between different pretrained models

--- httpd/handler/tokenizer_post.go
+++ httpd/handler/tokenizer_post.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sugarme/tokenizer"
 	"github.com/sugarme/tokenizer/pretrained"
 )
 
@@ -13,12 +14,29 @@ type tokenizerRequest struct {
 	Model string `json:"model"`
 }
 
+type model interface {
+	EncodeSingle(input string, addSpecialTokensOpt ...bool) (*tokenizer.Encoding, error)
+}
+
+func getModelSwitch(model_string string) model {
+	switch model_string {
+	case "bert":
+		return pretrained.BertBaseUncased()
+	case "gpt2":
+		return pretrained.GPT2(true, true)
+	case "roberta":
+		return pretrained.RobertaBase(true, true)
+	default:
+		return nil
+	}
+}
+
 func TokenizerPost() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tk := pretrained.BertBaseUncased()
-
 		requestBody := tokenizerRequest{}
 		c.Bind(&requestBody)
+
+		tk := getModelSwitch(requestBody.Model)
 
 		en, err := tk.EncodeSingle(requestBody.Text)
 		if err != nil {
