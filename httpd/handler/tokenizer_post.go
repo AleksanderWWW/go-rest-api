28d1faa5642398079c438cf51cf4b3da6b12f1e3
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-01-30 22:43:03 +0100

    move backend code to seperate package

--- httpd/handler/tokenizer_post.go
+++ httpd/handler/tokenizer_post.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sugarme/tokenizer"
-	"github.com/sugarme/tokenizer/pretrained"
+
+	"github.com/AleksanderWWW/tokenizer-api/backend/tokenizer"
 )
 
 type tokenizerRequest struct {
@@ -16,29 +16,12 @@ type tokenizerRequest struct {
 	TrimOffsets    bool   `json:"trimOffsets"`
 }
 
-type model interface {
-	EncodeSingle(input string, addSpecialTokensOpt ...bool) (*tokenizer.Encoding, error)
-}
-
-func getModelSwitch(model_string string, addPrefixSpace bool, trimOffsets bool) model {
-	switch model_string {
-	case "bert":
-		return pretrained.BertBaseUncased()
-	case "gpt2":
-		return pretrained.GPT2(trimOffsets, trimOffsets)
-	case "roberta":
-		return pretrained.RobertaBase(trimOffsets, trimOffsets)
-	default:
-		return nil
-	}
-}
-
 func TokenizerPost() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		requestBody := tokenizerRequest{}
 		c.Bind(&requestBody)
 
-		tk := getModelSwitch(requestBody.Model, requestBody.AddPrefixSpace, requestBody.TrimOffsets)
+		tk := tokenizer.GetModelSwitch(requestBody.Model, requestBody.AddPrefixSpace, requestBody.TrimOffsets)
 
 		en, err := tk.EncodeSingle(requestBody.Text)
 		if err != nil {
