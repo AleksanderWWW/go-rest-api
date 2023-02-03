f53780d88556882c2c7f99d805609f1b1b060821
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-02-03 22:31:39 +0100

    add jwt verification with cookies

--- httpd/main.go
+++ httpd/main.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/AleksanderWWW/tokenizer-api/httpd/handler"
+	"github.com/AleksanderWWW/tokenizer-api/httpd/middleware"
 )
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/status", handler.StatusGet())
-	r.POST("/tokenize", handler.TokenizerPost())
+	r.POST("/tokenize", middleware.RequireAuth, handler.TokenizerPost())
 	r.POST("/login", handler.Login())
 
 	r.Run()
---
+++ httpd/middleware/require_auth.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func RequireAuth(c *gin.Context) {
+	tokenString, err := c.Cookie("Authorization")
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header)
+		}
+		return []byte("56743985498282154984553094"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		if claims["sub"] != "myemail@email.com" {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+	} else {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	c.Next()
+}
