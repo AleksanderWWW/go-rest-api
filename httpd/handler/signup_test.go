fc8872d525ee3410b337ec1e56fc47ec300cce7c
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-03-04 17:59:39 +0100

    add tests for signup endpoint

---
+++ httpd/handler/signup_test.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-playground/assert"
+)
+
+func TestSignup(t *testing.T) {
+	mockResponse := `{"email":"test@test.com","status":"success"}`
+
+	r := SetUpRouter()
+	r.POST("/signup", SignUp())
+
+	reqData := []byte(`{"email":"test@test.com", "password": "password"}`)
+
+	req, _ := http.NewRequest("POST", "/signup", bytes.NewBuffer(reqData))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	responseData, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, mockResponse, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
+}
