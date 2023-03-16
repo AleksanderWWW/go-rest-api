60a53edebd2aa0d0a4c2fae8b595aba3c05c8563
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-03-16 15:12:29 +0100

    use cors

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
@@ -16,6 +17,14 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST"},
+		AllowHeaders:     []string{"Origin, Content-Type, Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	r.GET("/status", handler.StatusGet())
 	r.POST("/tokenize", middleware.RequireAuth, handler.TokenizerPost())
